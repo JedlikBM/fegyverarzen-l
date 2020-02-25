b89dec0ee77dbd8f46a26c0e1825b5368f143117
Bódai.Martin <martinbodai24@gmail.com>
2020-02-25 13:17:07 +0100

    létrehoztuk a fegyvertár listát

--- fegyverarzenál/Program.cs
+++ fegyverarzenál/Program.cs
@@ -8,8 +8,18 @@ namespace fegyverarzenál
 {
     class Program
     {
+        enum fegyverek { apaökle, uzi, m4, gránát, lánszóró, atombomba, fing }
+
         static void Main(string[] args)
         {
+            fegyverek kezemben = fegyverek.atombomba;
+            List<fegyverek> fegyvertár = new List<fegyverek>();
+            fegyvertár.Add(fegyverek.uzi);
+            fegyvertár.Add(fegyverek.fing);
+            foreach (fegyverek elem in fegyvertár)
+            {
+                Console.WriteLine(elem);
+            }
 
             Console.ReadKey();
         }
